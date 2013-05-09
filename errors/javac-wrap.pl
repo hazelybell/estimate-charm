3ee549c4202cb77466b6913a5608df2a9e576e09
Joshua Campbell <buttercup>
2013-05-08 20:10:16 -0600

    Test instrumentation rework.

--- errors/javac-wrap.pl
+++ errors/javac-wrap.pl
@@ -11,6 +11,9 @@ use IO::Handle;
 use List::Util qw(max min);
 $SIG{PIPE} = 'IGNORE';
 use ZeroMQ qw/:all/;
+use Data::Dumper;
+use Digest::MD5 qw(md5 md5_hex md5_base64);
+use List::Util;
 
 my $real_javac = $ENV{JAVAC_WRAPPER_REAL_JAVAC} ? $ENV{JAVAC_WRAPPER_REAL_JAVAC} : qx{which javac};
 chomp $real_javac;
@@ -46,26 +49,28 @@ for (@inputfiles) {
   print LOGFILE;
 }
 
-my $status;
 my $main_compile_status;
-my %files_mentioned;
 my %possible_bad_files;
 
 sub attempt_compile {
-  print LOGFILE join(' ', @_);
+#   print LOGFILE join(' ', @_);
   my $ccout;
   my $pid = open3("<&STDIN", $ccout, $ccout, $real_javac, @_) or die $?;
   my $compile_error = 0;
+  my %files_mentioned;
+  my $status;
+  my @results;
 
   while (<$ccout>) {
     chomp;
     print $_;
     if (m/\.java/i) {
       print LOGFILE "javac said: " . $_;
-      my ($file) = ($_ =~ m/(\S+\.java):/i);
-      print LOGFILE "Possible error locatplotion: $file";
+      my ($file, $line) = ($_ =~ m/(\S+\.java):(\d+):/i);
+      print LOGFILE "Possible error location: $file";
       $files_mentioned{$file}++;
       $compile_error++;
+      push @results, [$file, $line];
     }
   }
   print "waiting...";
@@ -76,7 +81,7 @@ sub attempt_compile {
   if ($status) {
     $compile_error++;
   }
-  return !$compile_error;  
+  return (!$compile_error, $status, \%files_mentioned, \@results);
 }
 
 my ($lmout, $lmerr, $lmin, $lmpid);
@@ -95,16 +100,16 @@ sub startMITLM {
 	last;
       }
   }
-  print $lmin "for ( i =$/";
+  print $lmin "for ( i =";
   print $!;
-  print STDERR "made it";
   my $i = 0;
   while(my $line = <$lmout>) {
       chomp($line);
       print "MITLM said $line";
       $i++ if ($line =~ m/Live Entropy ([-\d.]+)/);
-      last if $i >= 2;
+      last if $i >= 1;
   }
+  print STDERR "MITLM seems okay";
 }
 
   sub javaCommentHack {
@@ -162,19 +167,31 @@ $socket->connect( "tcp://127.0.0.1:32132" ); # java lexer
         return $outst;
     };
 
-
-
- sub lexAfile {
-     my ($file) = @_;
-     open INPUTFILE, '<', $file or die;
-     my $slurped = '';
-     while (<INPUTFILE>) {
+sub slurpAFile {
+  my ($file) = @_;
+  open INPUTFILE, '<', $file or die;
+  my $slurped = '';
+  while (<INPUTFILE>) {
 #       s/\n//gs;
-# 	$lexed .= lex($_);
-	$slurped .= $_;
-     }
-     close INPUTFILE;
-     return lex($slurped);
+#       $lexed .= lex($_);
+    $slurped .= $_;
+  }
+  close INPUTFILE;
+  return $slurped;
+}
+
+sub replaceAFile {
+  local $/ = '';
+  local $\ = '';
+  my ($file, $text) = @_;
+  open OUTPUTFILE, '>', $file or die;
+  print OUTPUTFILE $text or die;
+  close OUTPUTFILE or die;
+}
+
+sub lexAfile {
+    my ($file) = @_;
+    return lex(slurpAFile($file));
 }
 
 sub toksToTrain {
@@ -203,8 +220,9 @@ sub toksToCode {
   return $code;
 }
 
-sub findNworst {
-  my (@toks) = @_;
+sub findNWorst {
+  my ($toks) = @_;
+  my @toks = @$toks;
   my @possibilities;
   for (my $i = 0; $i < ($#toks-($block)); $i += $step) {
 #     print STDOUT toksToQuery([ @toks[$i..$i+($block-1)] ]);
@@ -218,108 +236,134 @@ sub findNworst {
     push @possibilities, [ [ @toks[$i..$i+($block-1)] ], $entropy ];
   }
   @possibilities = sort { $b->[1] <=> $a->[1] } @possibilities;
-  return @possibilities;
+  return \@possibilities;
+}
+
+sub printNWorst {
+  my ($worst) = @_;
+  for my $i (0..$N) {
+#     print (STDERR Dumper $worst);
+    print(STDERR "Check near " . $worst->[$i][0][0][1].":".$worst->[$i][0][0][2]
+      . " to " . $worst->[$i][0][$#{$worst->[$i][0]}][1].":".$worst->[$i][0][$#{$worst->[$i][0]}][2]);
+#         my $code = join('', @{$worst[$i][0]});
+#         print(STDERR "Check near " .$code);
+    print(STDERR "With entropy " . $worst->[$i][1]);
+  }
 }
 
 unless ($validate) {
-  unless (attempt_compile(@ARGV) || $forcetrain) {
-    %possible_bad_files = %files_mentioned;
-    $main_compile_status = $status;
-    startMITLM();
-    print LOGFILE "FAIL";
-    # we need to determine exactly which file failed because of this dumb compile a ton at once bs
-    for my $source (keys(%possible_bad_files)) {
-      unless (attempt_compile(@ARGV_NO_LISTS, $source)) {
-	print(STDERR "Maybe the error was in $source?");
-	my @toks = @{lexAfile($source)};
-	print STDERR "Slurped " . @toks . " tokens.";
-	
-	my @worst = findNworst(@toks);
-	for my $i (0..$N) {
-          print(STDERR "Check near " . $worst[$i][0][0][1].":".$worst[$i][0][0][2]
-            . " to " . $worst[$i][0][$#{$worst[$i][0]}][1].":".$worst[$i][0][$#{$worst[$i][0]}][2]);
-# 	  my $code = join('', @{$worst[$i][0]});
-# 	  print(STDERR "Check near " .$code);
-	  print(STDERR "With entropy " . $worst[$i][1]);
-	}
+    my ($success, $status, $files_mentioned);
+    ($success, $status, $files_mentioned) = attempt_compile(@ARGV) unless $forcetrain;
+    unless ($success || $forcetrain) {
+      %possible_bad_files = %$files_mentioned;
+      $main_compile_status = $status;
+      startMITLM();
+      print LOGFILE "FAIL";
+      # we need to determine exactly which file failed because of this dumb compile a ton at once bs
+      for my $source (keys(%possible_bad_files)) {
+        unless (attempt_compile(@ARGV_NO_LISTS, $source)) {
+          print(STDERR "Maybe the error was in $source?");
+          my @toks = @{lexAfile($source)};
+          print STDERR "Slurped " . @toks . " tokens.";
+          
+          printNWorst(findNWorst(@toks));
+        }
       }
-    }
-  } else {
-    print LOGFILE "COMPILE OK";
-    print "COMPILE OK";
-    $status = 0;
-    my $lexed = '';
+    } else {
+      print LOGFILE "COMPILE OK";
+      print "COMPILE OK";
+      $status = 0;
+      my $lexed = '';
       for my $file (@inputfiles) {
-	  print TOKFILE toksToTrain(lexAfile($file));
+          print TOKFILE toksToTrain(lexAfile($file));
       }
     }
   } else {
+    my ($validateMode, $validateNumber) = split(' ', $validate);
+    my $testlogfile = "/tmp/javac-testlog-".$validateMode."-".substr(md5_base64(join(chr(0),@inputfiles)),0,5);
+    open TESTLOG, '>>', $testlogfile or die;
     startMITLM();
-    my $correct = 0;
+    my $oursum = 0;
+    my $jcsum = 0;
+    my $combineda = 0;
+    my $combinedb = 0;
     my $tries = 0;
     for my $source (@inputfiles) {
-      my @toks = split(' ', lexAfile($source));
+      my $originalsource = slurpAFile($source);
+      my @toks = @{lex($originalsource)};
       print STDERR "Slurped " . @toks . " tokens.";
       if (scalar(@toks) < 10) { next; }
-      my @worst = findNworst(@toks);
-      my @worst = findNworst(@toks);
-      for my $i (0..$N) {
-        my $code = join('', @{$worst[$i][0]});
-        $code =~ s/<SPACE>/ /g;
-        print(STDERR "Check near " .$code);
-        print(STDERR "With entropy " . $worst[$i][1]);
-      }
-      my @thresh;
-      for my $i (0..$#worst) {
-	$thresh[$i] = $worst[$i][1];
-      }
-      print STDERR "THRESHOLD $thresh[0]";
-      my ($validateMode, $validateNumber) = split(' ', $validate);
+      my $worst = findNWorst(\@toks);
+      printNWorst($worst);
+      my @worst = @$worst;
       my %possibleToks = ();
       for (@toks) {
-	$possibleToks{$_}++;
+	$possibleToks{$_->[0]}++;
       }
       my @possibleToks = keys(%possibleToks);
       for my $i (1..$validateNumber) {
 	my @mutatedToks = @toks;
 	my $loc = int(rand($#mutatedToks));
+	my $mutline = $toks[$loc][1];
+	my $mutchar = $toks[$loc][2];
+# 	die "$mutline:$mutchar";
 	if ($validateMode =~ m/d/) {
 	  splice(@mutatedToks, $loc, 1);
 	} elsif ($validateMode =~ m/r/) {
-	  splice(@mutatedToks, $loc, 1, $possibleToks[int(rand($#possibleToks))]);
+	  splice(@mutatedToks, $loc, 1, [$possibleToks[int(rand($#possibleToks))], $mutline, $mutchar]);
 	} elsif ($validateMode =~ m/i/) {
-	  splice(@mutatedToks, $loc, 0, $possibleToks[int(rand($#possibleToks))]);
+	  splice(@mutatedToks, $loc, 0, [$possibleToks[int(rand($#possibleToks))], $mutline, $mutchar]);
 	} elsif ($validateMode =~ m/R/) {
-	  splice(@mutatedToks, $loc, 1, " XXXXXXXX ");
+	  splice(@mutatedToks, $loc, 1, [" XXXXXXXX ", $mutline, $mutchar]);
 	} elsif ($validateMode =~ m/I/) {
-	  splice(@mutatedToks, $loc, 0, " XXXXXXXX ");
+	  splice(@mutatedToks, $loc, 0, [" XXXXXXXX ", $mutline, $mutchar]);
 	} else {
 	  die "what?"
 	}
-        my $code = join('', @toks[$loc-$order..$loc+$order]);
-        $code =~ s/<SPACE>/ /g;
-        print(STDERR "Error inserted near " .$code);
-	my @mutatedWorst = findNworst(@mutatedToks);
-	$tries++;
-	my $at = $N;
-	for my $i (0..$#mutatedWorst) {
-	  if ($thresh[$i] < $mutatedWorst[$i][1]) {
-	    print "$i";
-            $code = join('', @{$mutatedWorst[$i][0]});
-            $code =~ s/<SPACE>/ /g;
-            print(STDERR "Check near " .$code);
-            print(STDERR "With entropy " . $mutatedWorst[$i][1]);
-            $at = $i;
-	    last;
-	  }
+	replaceAFile($source, toksToCode(\@mutatedToks));
+	print STDERR "Mutated $source:$mutline:$mutchar";
+	my $ourResults = findNWorst(\@mutatedToks);
+	printNWorst($ourResults);
+        my $ourindex = 0;
+	foreach(@$ourResults) {
+          $ourindex++;
+          if ($_->[0][0][1] <= $mutline) {
+            if ($_->[0][$#{$_->[0]}][1] >= $mutline) {
+              if ($_->[0][0][1] < $mutline || $_->[0][0][2] <= $mutchar) {
+                if ($_->[0][$#{$_->[0]}][1] > $mutline || $_->[0][$#{$_->[0]}][2] >= $mutchar) {
+                  print "Correct: $ourindex";
+                  last;
+                }
+              }
+            }
+          }
 	}
-	$correct++ if ($at < $N);
-	print STDERR "Accuracy: " . $correct/$tries;
+        my ($success, $status, $files_mentioned, $javacresults) = attempt_compile(@ARGV);
+        next if $success;
+        my $jcindex = 0;
+        foreach(@$javacresults) {
+          $jcindex++;
+          if ($_->[0] =~ m/$source/) {
+            if ($_->[1] == $mutline) {
+              last;
+            }
+          }
+        }
+        $jcsum += 1.0/$jcindex;
+        $oursum += 1.0/$ourindex;
+        $combineda += max(1.0/(2.0*$jcindex-1), 1.0/(2.0*$ourindex));
+        $combinedb += max(1.0/(2.0*$jcindex), 1.0/(2.0*$ourindex-1));
+	$tries += 1;
+	replaceAFile($source, $originalsource);
+        print STDERR "Our MRR: " . $oursum/$tries;
+        print STDERR "JavaC MRR: " . $jcsum/$tries;
+        print STDERR "JavaC,Our MRR: " . $combineda/$tries;
+        print STDERR "Our,JavaC MRR: " . $combinedb/$tries;
       }
    }
 }
 
-for (%files_mentioned) { print LOGFILE; }
+for (%possible_bad_files) { print LOGFILE; }
 
 close LOGFILE;
 defined ($lmin) and close $lmin;
@@ -328,4 +372,4 @@ defined ($lmout) and close $lmout;
 $socket->close();
 $ctxt->term();
 
-exit $status;
+exit $main_compile_status;

44007d8bfdb1b285ab1362860f88346768b19543
Joshua Campbell <buttercup>
2013-08-26 14:09:33 -0600

    burgers

--- python/lexPythonMQ.py
+++ python/lexPythonMQ.py
@@ -2,3 +2,11 @@
 
 import tokenize;
 import zmq;
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://lo:32132")
+
+while True:
+    # Wait for next request from client
+    message = socket.recv()

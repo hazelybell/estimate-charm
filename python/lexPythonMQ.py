5bfbaf0036b8a54693e320b473899b7497c816c0
Alex Wilson <aewilson@ualberta.ca>
2013-08-26 16:28:02 -0600

    In lexPythonMQ, return dicts not tuples

--- python/lexPythonMQ.py
+++ python/lexPythonMQ.py
@@ -1,14 +1,34 @@
 #!/usr/bin/python
 
-import re, sys, tokenize, zmq;
+import sys, token, tokenize, zmq;
 from StringIO import StringIO
 
 def err(msg):
 	sys.err.write(str(msg) + '\n')
+
+
+class LexPy(object):
 	
+	def __init__(self):
+		pass
+
+	def lex(code):
+		def tup_to_dict(tup):
+			# tup = [type, val, [startrow, col], [endrow, col], line]
+			return {
+				'type': token.tok_name[tup[0]],
+				'value': tup[1],
+				'start': tup[2],
+				'end': tup[3],
+			}
+
+		return [tup_to_dict(tok) for tok in \
+			tokenize.generate_tokens(StringIO(code).readline)]
+		
 
 class LexPyMQ(object):
-	def __init__(self):
+	def __init__(self, lexer):
+		self.lexer = lexer
 		self.zctx = zmq.Context()
 		self.socket = self.zctx.socket(zmq.REP)
 
@@ -21,7 +41,7 @@ class LexPyMQ(object):
 			if not msg.get('python'):
 				err('received non-python code')
 			code = msg.get('body', '')
-			self.socket.send_json(tokenize.generate_tokens(StringIO(code)))
+			self.socket.send_json(self.lexer.lex(code))
 
 if __name__ == '__main__':
-	LexPyMQ().run()
+	LexPyMQ(LexPy()).run()

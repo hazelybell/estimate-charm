ab261c6665b1a74cf91b498dc42c7863dbe171fa
Joshua Charles Campbell <gitcommit@orezpraw.com>
2013-10-10 17:10:25 -0600

    forgot to add a file

---
+++ python/sourceModel.py
@@ -0,0 +1,82 @@
+#    Copyright 2013 Joshua Charles Campbell
+#
+#    This file is part of UnnaturalCode.
+#    
+#    UnnaturalCode is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    UnnaturalCode is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with UnnaturalCode.  If not, see <http://www.gnu.org/licenses/>.
+
+from ucUtil import *
+from mitlmCorpus import *
+from pythonLexical import *
+from operator import itemgetter
+
+class sourceModel(object):
+    
+    def __init__(self, cm=mitlmCorpus(), lm=pythonLexical(), windowSize=20):
+        self.cm = cm
+        self.lm = lm
+        self.windowSize = windowSize
+    
+    def trainFile(self, files):
+        """Blindly train on a set of files whether or not it compiles..."""
+        files = [files] if isinstance(files, str) else files
+        assert isinstance(files, list)
+        for fi in files:
+            sourceCode = slurp(fi)
+            self.trainString(sourceCode)
+            
+    def stringifySource(self, sourceCode):
+        """Convert source code to a list of strings"""
+        return self.stringifyAll(self.lm.lex(sourceCode))
+    
+    def stringifyAll(self, lexemes):
+        """Clean up a list of lexemes and convert it to a list of strings"""
+        return map(self.lm.stringify1, self.lm.scrub(lexemes))
+
+    def corpify(self, lexemes):
+        """Corpify a string"""
+        return self.cm.corpify(self.stringifyAll(lexemes))
+    
+    def trainString(self, sourceCode):
+        """Train on a source code string"""
+        return self.cm.addToCorpus(self.stringifySource(sourceCode))
+    
+    def queryString(self, sourceCode):
+        return self.cm.queryCorpus(self.stringifySource(sourceCode))
+    
+    def queryLexed(self, lexemes):
+        return self.cm.queryCorpus(self.stringifyAll(lexemes))
+    
+    def windowedQuery(self, lexemes):
+        lastWindowStarts = len(lexemes)-self.windowSize-1
+        r = []
+        for i in range(0,lastWindowStarts):
+            end = i+self.windowSize
+            w = lexemes[i:end]
+            e = self.queryLexed(w)
+            r.append( (w,e) )
+        return r
+    
+    def worstWindows(self, lexemes):
+        unsorted = self.windowedQuery(lexemes)
+        return sorted(unsorted, key=itemgetter(1), reverse=True)
+    
+    def release(self):
+        self.cm.release()
+    
+    def __del__(self):
+        """I am a destructor, but release should be called explictly."""
+        self.release
+
+# rwfubmqqoiigevcdefhmidzavjwg
+        

382c422b23dfceb737fde372794fcfd0f400a6be
Joshua Charles Campbell <joshua2@ualberta.ca>
2014-11-25 16:49:58 -0700

    fix copyright

--- unnaturalgrams/copper.c
+++ unnaturalgrams/copper.c
@@ -1,20 +1,20 @@
 /* copper.c Debugging helper.
- * This file is a part of the Copper framework.
- * Copyright 2006-2008 Joshua Charles Campbell.
-
- * The Copper framework is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 3
- * of the License, or (at your option) any later version.
-
- * This program is distributed in the hope that it will be useful,
+ * Copyright 2006, 2007, 2008, 2013, 2014 Joshua Charles Campbell
+ *
+ * This file is part of UnnaturalCode.
+ *
+ * UnnaturalCode is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * UnnaturalCode is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
-
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with UnnaturalCode.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "copper.h"
 #include <string.h>

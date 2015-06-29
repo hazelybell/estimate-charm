2eaf68d873908550823d6f45ce72c0cd993aae12
Joshua Charles Campbell <joshua2@ualberta.ca>
2015-06-29 15:02:16 -0600

    fix copyrights and names etc.

--- estimatecharm/ucUtil.py
+++ estimatecharm/ucUtil.py
@@ -1,19 +1,19 @@
-#    Copyright 2013, 2014 Joshua Charles Campbell
+#    Copyright 2013, 2014, 2015 Joshua Charles Campbell
 #
-#    This file is part of UnnaturalCode.
+#    This file is part of EstimateCharm.
 #    
-#    UnnaturalCode is free software: you can redistribute it and/or modify
+#    EstimateCharm is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
-#    UnnaturalCode is distributed in the hope that it will be useful,
+#    EstimateCharm is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with UnnaturalCode.  If not, see <http://www.gnu.org/licenses/>.
+#    along with EstimateCharm.  If not, see <http://www.gnu.org/licenses/>.
 # ------- UTILITY FUNCTIONS ---------------------------------------------------
 
 import os, sys, re, json
